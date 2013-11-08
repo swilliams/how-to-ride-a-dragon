cfbf5aa219285c7bdd59aeedfe22044e3fc54ca0
Scott Williams <scott@swilliams.me>
2013-11-08 14:39:59 -0700

    Added Coords. Added math to move the dragon

--- lib/harness.rb
+++ lib/harness.rb
@@ -10,6 +10,24 @@ class Fixnum
   def minutes
     self * 60
   end
+
+  def to_radians
+    self * Math::PI / 180
+  end
+end
+
+class Coords
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    self.x = x
+    self.y = y
+    self.z = z
+  end
+
+  def to_s
+    "{x: #{self.x}, y: #{self.y}, z: #{self.z}}"
+  end
 end
 
 module Harness
@@ -25,6 +43,28 @@ module Harness
 
   def fly(duration = 1.minutes, &block)
     @harness_animal.fly(duration, &block)
+    # calculate distance
+    # total distance traveled = speed * time
+    # ground distance traveled = total_distance * cos(angle)
+
+    total_dist = total_dist_traveled(@harness_animal.land_speed, duration)
+    ground_dist = calculate_adjacent(total_dist, @harness_animal.angle).floor
+    x = calculate_adjacent(ground_dist, @harness_animal.direction).floor
+    y = calculate_opposite(ground_dist, @harness_animal.direction).floor
+    z = calculate_adjacent(total_dist, @harness_animal.angle).floor
+    @harness_animal.coords = Coords.new x, y, z
+  end
+
+  def total_dist_traveled(speed, time)
+    speed * time
+  end
+
+  def calculate_adjacent(hyp, angle)
+    hyp * Math::cos(angle.to_radians)
+  end
+
+  def calculate_opposite(hyp, angle)
+    hyp * Math::sin(angle.to_radians)
   end
 
   module PersonInstanceMethods
@@ -41,7 +81,7 @@ module Harness
   end
 
   module AnimalInstanceMethods
-    attr_accessor :flying, :direction, :angle, :speed, :duration
+    attr_accessor :flying, :direction, :angle, :land_speed, :duration, :coords
 
     def flying?
       self.flying
@@ -62,7 +102,7 @@ module Harness
     end
 
     def speed(s)
-      self.speed = s
+      self.land_speed = s
     end
   end
 end
